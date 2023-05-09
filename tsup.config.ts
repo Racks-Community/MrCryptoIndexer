880452f25c1debc669eb67990eadcfd634360768
DanielDS <ddsintim@inf.upv.es>
2023-05-09 13:00:35 +0200

    build: :package: config tsup

---
+++ tsup.config.ts
@@ -0,0 +1,10 @@
+import { defineConfig } from "tsup";
+
+export default defineConfig({
+  entry: ["src/server.ts"],
+  bundle: true,
+  minify: true,
+  dts: true,
+  clean: true,
+  target: "node18",
+});
