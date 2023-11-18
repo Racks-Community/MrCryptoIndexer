7d30a810e653346dab2634a7abb3dd126f54a791
Daniel Sintimbrean <102896754+DanielSintimbrean@users.noreply.github.com>
2023-11-19 00:30:28 +0100

    Update Mr. Crypto Indexer configuration and documentation

--- web/astro.config.mjs
+++ web/astro.config.mjs
@@ -6,21 +6,25 @@ import tailwind from "@astrojs/tailwind";
 export default defineConfig({
   integrations: [
     starlight({
-      title: "Docs with Tailwind",
+      title: "Mr. Crypto Indexer",
       social: {
-        github: "https://github.com/withastro/starlight",
+        github: "https://github.com/Racks-Community/MrCryptoIndexer/",
+        "x.com": "https://x.com/mrcryptobyracks",
+      },
+      editLink: {
+        baseUrl:
+          "https://github.com/Racks-Community/MrCryptoIndexer/edit/main/web",
       },
       sidebar: [
         {
           label: "Introducción",
           items: [
             // Each item here is one entry in the navigation menu.
-            { label: "Motivación", link: "/introduccion/motivacion" },
+            {
+              label: "Motivación",
+              link: "/introduccion/motivacion",
+            },
           ],
-        },
-        {
-          label: "Reference",
-          autogenerate: { directory: "reference" },
         },
       ],
       customCss: ["./src/tailwind.css"],
--- web/tailwind.config.mjs
+++ web/tailwind.config.mjs
@@ -1,17 +1,30 @@
-import colors from "tailwindcss/colors";
 import starlightPlugin from "@astrojs/starlight-tailwind";
+
+// Generated color palettes
+const accent = {
+  200: "#d0c0ea",
+  500: "#864ac4",
+  600: "#8247c0",
+  900: "#3c2458",
+  950: "#2a1c3c",
+};
+const gray = {
+  100: "#f5f6f8",
+  200: "#eceef2",
+  300: "#c0c2c7",
+  400: "#888b96",
+  500: "#545861",
+  700: "#353841",
+  800: "#24272f",
+  900: "#17181c",
+};
 
 /** @type {import('tailwindcss').Config} */
 export default {
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
   theme: {
     extend: {
-      colors: {
-        // Your preferred accent color. Indigo is closest to Starlight’s defaults.
-        accent: colors.indigo,
-        // Your preferred gray scale. Zinc is closest to Starlight’s defaults.
-        gray: colors.zinc,
-      },
+      colors: { accent, gray },
     },
   },
   plugins: [starlightPlugin()],
