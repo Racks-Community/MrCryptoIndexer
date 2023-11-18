3c136926a07e0243627f62b4727b30ab76421071
Daniel Sintimbrean <102896754+DanielSintimbrean@users.noreply.github.com>
2023-11-18 20:32:31 +0100

    refactor: Update MrCrypto model fields: totalScore -> rarityScore, ranking -> rarityRanking

--- src/schema/holder.ts
+++ src/schema/holder.ts
@@ -1,11 +1,45 @@
 import { builder } from "@/builder";
 import { prisma } from "@/db";
 
+const AscendDescentEnum = builder.enumType("AscendDescentByEnum", {
+  values: ["asc", "desc"] as const,
+});
+
+const MrCryptoOrderByEnum = builder.enumType("MrCryptoOrderByEnum", {
+  values: ["rarityRanking", "tokenId"] as const,
+});
+
 builder.prismaObject("Holder", {
   fields: (t) => ({
     address: t.exposeString("address"),
     mrCryptosOwned: t.relation("MrCryptosOwned", {
-      query: { orderBy: { tokenId: "asc" } },
+      args: {
+        first: t.arg.int({ required: true, defaultValue: 100 }),
+        skip: t.arg.int({ required: true, defaultValue: 0 }),
+        order: t.arg({
+          required: true,
+          defaultValue: { type: "desc", by: "tokenId" },
+          type: builder.inputType("MrCryptoOrderBy", {
+            fields: (t) => ({
+              type: t.field({
+                type: AscendDescentEnum,
+                required: true,
+                defaultValue: "desc",
+              }),
+              by: t.field({
+                type: MrCryptoOrderByEnum,
+                required: true,
+                defaultValue: "tokenId",
+              }),
+            }),
+          }),
+        }),
+      },
+      query: (args) => ({
+        orderBy: { [args.order.by]: args.order.type },
+        take: args.first,
+        skip: args.skip,
+      }),
     }),
     numberOfMrCryptos: t.field({
       select: {
